f0e0d49815fe087436bf7b3eb5c57c26d4777954
JasonDeras <derasjason2@gmail.com>
2019-04-26 13:32:55 -0600

    Creacion del menu y las opciones del login

--- lab2/src/lab2/Lab2.java
+++ lab2/src/lab2/Lab2.java
@@ -1,21 +1,102 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab2;
 
-/**
- *
- * @author jason
- */
+import java.util.*;
+
 public class Lab2 {
 
-    /**
-     * @param args the command line arguments
-     */
+    static Scanner l = new Scanner(System.in);
+
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        String usuario = "";
+        String contra = "";
+        System.out.println("Menu\n");
+        System.out.println("1. Registro de Empleados");
+        System.out.println("2. Despedir Empleados");
+        System.out.println("3. Log In");
+        System.out.println("4. Ascender Cajero");
+        System.out.println("5. Listar Empleados");
+        System.out.println("6. Modificar Empleado");
+        System.out.println("7. Random\n");
+        System.out.print("Ingrese una opcion: ");
+        int op = l.nextInt();
+        System.out.println("");
+        while (op != 3) {
+            System.out.println("No puede ingresar sin antes login\n");
+            System.out.print("Ingrese su usuario: ");
+            usuario = l.next();
+            System.out.print("Ingrese su contraseña: ");
+            contra = l.next();
+            System.out.println("");
+            if (usuario.equals("leobanegas") && contra.equals("99")) {
+                System.out.println("1. Registro de Empleados");
+                System.out.println("2. Despedir Empleados");
+                System.out.println("3. Ascender Cajero");
+                System.out.println("4. Listar Empleados");
+                System.out.println("5. Modificar Empleado");
+                System.out.println("6. Random");
+                System.out.println("7. Salir del sistema\n");
+                System.out.print("Ingrese una opcion: ");
+                int op2 = l.nextInt();
+                System.out.println("");
+                switch (op2) {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        System.exit(0);
+                    default:
+                        System.out.println("Opcion no valida\n");
+                }//Fin del case de antes de hacer login
+            } else {
+                System.out.println("Contraseña o usuario incorrecto\n");
+            }
+        }//Fin del while que valida la opcion
+        while (op == 3) {
+            System.out.print("Ingrese su usuario: ");
+            usuario = l.next();
+            System.out.print("Ingrese su contraseña: ");
+            contra = l.next();
+            if (usuario.equals("leobanegas") && contra.equals("99")) {
+                System.out.println("1. Registro de Empleados");
+                System.out.println("2. Despedir Empleados");
+                System.out.println("3. Ascender Cajero");
+                System.out.println("4. Listar Empleados");
+                System.out.println("5. Modificar Empleado");
+                System.out.println("6. Random");
+                System.out.println("7. Salir del sistema\n");
+                System.out.print("Ingrese una opcion: ");
+                int op2 = l.nextInt();
+                System.out.println("");
+                switch (op2) {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        System.exit(0);
+                    default:
+                        System.out.println("Opcion no valida\n");
+                }//Fin del case de si escoge el login como primera opcion
+            } else {
+                System.out.println("Usuario o contraseña incorrecta");
+            }
+        }//While si la opcion es 3
+    }//Fin del main
 }
